742f8a1f4893de7e8d7a1f913db95f8c45c30bb6
ian k <kickthechair@gmail.com>
2021-09-01 21:04:46 +0100

    working set of basic fizzbuzz tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,22 +1,43 @@
 describe('Fizzbuzz', function() {
 
-  let fizzBuzz;
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
 
   describe('knows when a number is', function() {
     it('divisable by 3', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisableByThree(3)).toBe(true); // 3 is Fizz, 5 is Buzz & divis by 3 &5 = FizzBuzz
     });
   });
 
   describe('knows when a number is NOT', function() {
     it('divisable by 3', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisableByThree(1)).toBe(false);
     });
   });
 
+  describe('knows when a number is', () => {
+    it('divisable by 5', () => {
+     expect(fizzbuzz.isDivisableByFive(5)).toBe(true)
+    });
+  });
 
+  describe('knows when a number is NOT', () => {
+    it('divisable by 5', () => {
+      expect(fizzbuzz.isDivisableByFive(1)).toBe(false)
+    });
+  });
 
+  describe('knows when a number is', () => {
+    it('divisable by 15', () => {
+      expect(fizzbuzz.isDivisableByFifteen(15)).toBe(true)
+    });
+  });
+
+  describe('knows when a numbers is NOT', () => {
+    it('divisable by 15', () => {
+      expect(fizzbuzz.isDivisableByFifteen(1)).toBe(false)
+    });
+  });
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -2,4 +2,12 @@ class Fizzbuzz {
   isDivisableByThree(number) {
     return number % 3 === 0
   }
+
+  isDivisableByFive(number) {
+    return number % 5 === 0
+  }
+
+  isDivisableByFifteen(number) {
+    return number % 15 === 0
+  }
 }
